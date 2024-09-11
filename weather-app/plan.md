4cdcd011b92905fc7eef5caba68a1feba5903296
Stace0x <167257706+Stace0x@users.noreply.github.com>
2024-09-11 12:29:05 +0100

    basic HTML created with text field and button, STEP 5 planning added

--- weather-app/plan.md
+++ weather-app/plan.md
@@ -13,8 +13,14 @@
 
 4. make a basic form?
 
-- text field
-- search button
-- display field to show weather
+- text field✅
+- search button✅
+- display field to show weather✅
 
 5. Link to the API - fetch data
+  - handleChange function for text field
+  - onSubmiit function for search button
+  - create GET request using the imput text field 
+  - onSubmit fire the GET request
+  - store Json data from GET request in a STATE
+  - used state to display the chosen data
--- weather-app/src/App.tsx
+++ weather-app/src/App.tsx
@@ -4,7 +4,6 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
@@ -16,18 +15,20 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Weather</h1>
+      <section>
+        <form className="fields">
+          <label>
+            <input/>
+          </label>
+          <button type="submit">
+            Fetch the Weather
+          </button>
+        </form>
+      </section>
+      <section>
+        <p>clouds card</p>
+      </section>
     </>
   )
 }
